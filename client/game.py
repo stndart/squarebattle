3c4d2fb993aee2e4a141c916e0265e33105218ce
AndBondStyle <andbondstyle@gmail.com>
2017-02-14 16:38:34 +0300

    Game classes update

--- client/game.py
+++ client/game.py
@@ -1,44 +1,170 @@
 # Game logic implementation
 
 # Position aka Point
-class Position(tuple):
-    # (-1, -1) + (2, 3) = (1, 2)
-    def __add__(self):
-        pass
+class Position:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def __add__(self, other):
+        return Position(self.x + other.x, self.y + other.y)
+
 
 # Class for storing any masks (damage, support...)
 class Mask:
-    # Data - any rectangle (maybe square?) array with None (?) in center
     def __init__(self, data):
-        pass
-    
-    # Don't return anything, just change self
+        self.data = data
+        self.direction = 0
+        self.width, self.height = len(data[0]), len(data)
+        for x in range(self.width):
+            for y in range(self.height):
+                if self.data[y][x] is None:
+                    self.center = Position(x, y)
+                    break
+
     def rotate(self, direction):
-        pass
-    
-    # Return element with specified position
+        turns = abs(self.direction - direction)
+        for _ in range(turns):
+            self.data = list(zip(*self.data[::-1]))
+        if turns % 2 == 1:
+            self.width, self.height = self.height, self.width
+        self.direction = direction
+
     def get(self, position):
-        pass
-    
-    # Iterator of non-zero elements
+        position = self.center + position
+        return self.data[position.y][position.x]
+
     def __iter__(self):
-        pass
-        # for x in range(self.width):
-        #    for y in range(self.height):
-        #        if self.data[x][y]: yield self.data[x][y]
-        
+        for x in range(self.width):
+            for y in range(self.height):
+                if self.data[y][x]: yield (x, y), self.data[x][y]
+
+
 class Unit:
     def __init__(self, position):
         pass
-        
+
     def rotate(self, direction):
         pass
-    
+
     # Redirect additional damage from support (not sure about method name)
     def redirect(self, data):
         pass
-        
+
     def fight(self):
         pass
-        
-UNITS = {'unit': {'damage': Mask(), 'support': Mask(), 'childs': ['infantry', 'horseman']} # And so on
+
+
+# Please fill
+UNITS = {
+    'unit': {
+        'damage': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'support': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'children': ['infantry', 'horseman']
+    },
+    'infantry': {
+        'damage': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'support': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'children': ['pikeman', 'archer', 'warrior']
+    },
+    'horseman': {
+        'damage': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'support': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'children': ['dragoon', 'knight', 'hobbler']
+    },
+    'pikeman': {
+        'damage': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'support': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ])
+    },
+    'archer': {
+        'damage': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'support': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ])
+    },
+    'warrior': {
+        'damage': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'support': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ])
+    },
+    'dragoon': {
+        'damage': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'support': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ])
+    },
+    'knight': {
+        'damage': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'support': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ])
+    },
+    'hobbler': {
+        'damage': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]),
+        'support': Mask([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ])
+    }
+}
